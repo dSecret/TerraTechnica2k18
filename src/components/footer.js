663248f95631c8eaee677063a086b638c8fcac8d
Boot-Error <booterror99@gmail.com>
2018-02-17 15:30:40 +0530

    added links to the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,12 +7,9 @@ class Footer extends Component {
   render() {
     return (
         <div className="footer-wrapper">
-            <a href="#" className="fa fa-facebook"></a>
-            <a href="#" className="fa fa-twitter"></a>
-            <a href="#" className="fa fa-google"></a>
-            <a href="#" className="fa fa-linkedin"></a>
-            <a href="#" className="fa fa-youtube"></a>
-            <a href="#" className="fa fa-instagram"></a>
+            <a href="https://www.facebook.com/terratechnica/" className="fa fa-facebook"></a>
+            <a href="https://www.youtube.com/watch?v=mDUVf8YUVnQ" className="fa fa-youtube"></a>
+            <a href="https://www.instagram.com/terratechnica/" className="fa fa-instagram"></a>
         </div>
     );
   }
