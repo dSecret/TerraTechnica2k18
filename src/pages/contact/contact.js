f6d674a3079185a07da6ffda0c812c4749e93eed
rajnitd <36956425+rajnitd@users.noreply.github.com>
2018-03-01 18:23:32 +0530

    updated iframe attributes

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -37,9 +37,9 @@ class Contact extends Component {
           <div  id="iframewrap">
               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1747.430767469776!2d77.10345928463305!3d28.842982131809055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1b1923ada2e3%3A0x1169930518add2fe!2sNational+Institute+of+Technology+Delhi!5e0!3m2!1sen!2sin!4v1508233541308" 
                      width="100%" height="100%" 
-                     frameborder="0" 
+                     frameBorder="0" 
                      style={{border:"0"}} 
-                     allowfullscreen
+                     allowFullScreen
               >
               </iframe>
           </div>
