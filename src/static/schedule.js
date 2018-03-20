d64989d8bebc803938e40fbf8ebde55912ef0590
Boot-Error <booterror99@gmail.com>
2018-03-20 21:43:10 +0530

    Data for timeline added

--- src/static/schedule.js
+++ src/static/schedule.js
@@ -1,29 +1,127 @@
-const Db={
-	"day1":[
+const Db = {
+	"day1": [
 		{
-			"tit":"10:00 AM-10:30 PM",
-			"des":"Opening Ceremony - Audi"
-		},
-				{
-			"tit":"11:30 AM-12:30 PM",
-			"des":"Tata Crucible Hackathon Opening session -Audi"
+			"tit": "10:00AM - 10:30PM",
+			"des": "Opening Ceremony",
+			"loc": "Auditorium"
 		},
 		{
-			"tit":"10:00 AM-10:30 PM",
-			"des":"Opening Ceremony - Audi"
+			"tit": "11:30 AM-12:30 PM",
+			"des": "Tata Crucible Hackathon Opening session",
+			"loc": "Auditorium"
 		},
-				{
-			"tit":"10:00 AM-10:30 PM",
-			"des":"Opening Ceremony - Audi"
+		{
+			"tit": "11:30AM - 12:30PM",
+			"des": "Puck Collect", 
+			"loc": "Table Tennis Arena"
 		},
-				{
-			"tit":"10:00 AM-10:30 PM",
-			"des":"Opening Ceremony - Audi"
+		{
+			"tit": "12:30PM - 2:30PM",
+			"des": "Hackathon Prototype Evaluation",
+			"loc": "LT4/5"
 		},
-				{
-			"tit":"10:00 AM-10:30 PM",
-			"des":"Opening Ceremony - Audi"
+		{
+			"tit": "1:15PM - 2:00PM",
+			"des": "Guest Lecture by Mr. Chewang Norphel"
+			"loc": "Auditorium"
 		},
+		{
+			"tit": "2:00PM - 3:00PM",
+			"des": "ETS Quizathon",
+			"loc": "Auditorium"
+		},
+		{
+			"tit": "3:30PM - 5:30PM",
+			"des": "Hackathon closing session",
+			"loc": "Auditorium"
+		},
+		{
+			"tit": "3:30PM - 5:30PM",
+			"des": "Maze Runner",
+			"loc": "Table Tennis Arena"
+		},
+		{
+			"tit": "3:45PM - 4:45PM",
+			"des": "Blank Coding",
+			"loc": "Computer Center"
+		},
+		{
+			"tit": "ALL DAY",
+			"des": "Bull Ride",
+			"loc": "Behind Registration Desk"
+		},
+		{
+			"tit": "ALL DAY",
+			"des": "Zorb Ball",
+			"loc": "Echoes Ground"
+		}
+	],
+	"day2": [
+		{
+			"tit": "9:30AM - 10:30AM",
+			"des": "Clash of Codes",
+			"loc": "LT4"
+		},
+		{
+			"tit": "10:30AM - 11:30AM",
+			"des": "Guest Lecture by Maj. Gen. [Dr.] Gagandeep Bakshi",
+			"loc": "Auditorium"
+		},
+		{
+			"tit": "10:30AM - 12:30PM",
+			"des": "Fidelity Machine Learning Challenge",
+			"loc": "LT5"
+		},
+		{
+			"tit": "12:30PM - 1:30PM",
+			"des": "Guest Lecture by Mr. Gigi Joseph",
+			"loc": "Auditorium"
+		},
+		{
+			"tit": "1:30PM - 2:45PM",
+			"des": "Robo Soccer",
+			"loc": "Table Tennis Arena"
+		},
+		{
+			"tit": "1:30PM - 4:30PM",
+			"des": "Capture the flag",
+			"loc": "LT6"
+		},
+		{
+			"tit": "1:30PM - 4:30PM",
+			"des": "NITD Programming League",
+			"loc": "LT4/5"
+		},
+		{
+			"tit": "2:45PM - 4:45PM",
+			"des": "Death Race",
+			"loc": "Basket Ball Court"
+		},
+		{
+			"tit": "4:45PM - 5:30PM",
+			"des": "Robo Rumble",
+			"loc": "Table Tennis Arena"
+		},
+		{
+			"tit": "5:30PM - 6:00PM",
+			"des": "Closing Ceremony",
+			"loc": "Auditorium"
+		},
+		{
+			"tit": "6:00PM onwards",
+			"des": "EDM NIGHT",
+			"loc": "EDM Ground"
+		},
+		{
+			"tit": "ALL DAY",
+			"des": "Bull Ride",
+			"loc": "Behind Registration Desk"
+		},
+		{
+			"tit": "ALL DAY",
+			"des": "Paint Ball",
+			"loc": "Echoes Ground"
+		}
 	]
 
 }
